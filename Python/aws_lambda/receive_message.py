65a70e83b8de15f004b44ab598e1e6dd18982c07
Ariel Adonai Souza <arielrenostro@gmail.com>
2019-05-20 20:02:10 -0300

    Lambda recebimento de mensagem do API Gateway

---
+++ Python/aws_lambda/receive_message.py
@@ -0,0 +1,104 @@
+import json
+import os
+from datetime import datetime
+from decimal import Decimal
+
+import boto3
+
+
+def lambda_handler(event, context):
+    body = get_body(event)
+
+    data = {
+        'date': datetime.utcnow().strftime("%Y/%m/%dT%H:%M:%SZ"),
+        'car': body.get('car'),
+        'velocity': get_decimal(body, 'velocity'),
+        'rpm': get_decimal(body, 'rpm'),
+        'air_temp': get_decimal(body, 'airTemp'),
+        'oil_temp': get_decimal(body, 'oilTemp'),
+        'water_temp': get_decimal(body, 'waterTemp'),
+        'latitude': get_decimal(body, 'latitude'),
+        'longetude': get_decimal(body, 'longetude'),
+    }
+
+    data.update({
+        'id': f'{data["car"]}_{data["date"]}'
+    })
+
+    try:
+        dynamodb = boto3.resource(
+            'dynamodb',
+            aws_access_key_id=os.getenv('ACCESS_KEY'),
+            aws_secret_access_key=os.getenv('SECRET_KEY'),
+        )
+
+        table = dynamodb.Table(
+            os.getenv('DYNAMODB_TABLE')
+        )
+
+        table.put_item(
+            Item=data
+        )
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': {
+                'message': str(e)
+            }
+        }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(
+            get_json_data(data)
+        )
+    }
+
+
+def get_body(event):
+    if 'body' in event:
+        body = event['body']
+        if isinstance(body, str):
+            return json.loads(body)
+        elif isinstance(body, dict):
+            return body
+
+
+def get_decimal(body, key):
+    if key in body:
+        value = body[key]
+        try:
+            return Decimal(
+                str(value)
+            )
+        except:
+            pass
+
+
+def parse_value_json_data(value):
+    if isinstance(value, Decimal):
+        return str(value)
+    return value
+
+
+def get_json_data(data):
+    new_data = {}
+
+    for key, value in data.items():
+        if isinstance(value, dict):
+            value = get_json_data(value)
+
+        elif isinstance(value, (list, set)):
+            value_list = []
+            for v in value:
+                value_list.append(
+                    parse_value_json_data(v)
+                )
+            value = value_list
+        value = parse_value_json_data(value)
+
+        new_data.update({
+            key: value
+        })
+
+    return new_data
